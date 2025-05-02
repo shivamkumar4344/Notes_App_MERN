e0737e0c8baafc953b3061c2c6f54fbcc1d3cd0d
Shivam Kumar <ydvshivam10@gmail.com>
2025-05-02 12:21:51 +0530

    cors resolve

--- server/index.js
+++ server/index.js
@@ -21,12 +21,31 @@ const { authenticateToken } = require("./utilities");
 
 app.use(express.json());
 
-app.use(
-    cors({
-        origin: "*",
-        
-    })
-);
+// Handle CORS properly
+app.use((req, res, next) => {
+  const allowedOrigins = [
+    'https://notes-app-mern.vercel.app',
+    'http://localhost:5173',
+    'http://localhost:8000'
+  ];
+  
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
+  next();
+});
+
+// ... rest of your code ...
 
 app.get("/", (req, res) => {
     res.json({ 
